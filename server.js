5a85357353c374c484418c2e2845b2bf18867354
Rachidine567 <145989166+Rachidine567@users.noreply.github.com>
2024-03-04 01:48:38 -0800

    done

---
+++ server.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const mysql = require('mysql');
+const cors = require('cors');
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const db = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "register"
+})
+
+app.post('/register', (req, res) => {
+    const sql = "INSERT INTO login (`name`,`email`,`password`,`remember`) VALUES (?)";
+    const values = [
+        req.body.name,
+        req.body.email,
+        req.body.password,
+        req.body.remember
+    ]
+    db.query(sql, [values], (err, data) => {
+        if(err) {
+            return res.json("Error");
+        }
+        return res.json(data);
+    })
+})
+
+app.listen(8081, ()=> {
+    console.log("listening");
+})
